1dc29e654a66a89bb1d7e163320d35fe441222d2
Karst001 <ad.karsten@outlook.com>
2025-06-25 17:56:40 +0200

    boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import axios from 'axios';
 
 function App() {
+    const [PokemonData, setPokemonData] = useState([]);
+    const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
-  return (
-    <>
-      <h1>Gotta catch em all!</h1>
-    </>
-  )
+    useEffect(() => {
+        async function getPokemon() {
+            try {
+                const response = await axios.get(baseUrl);
+                console.log(response.data);
+                setPokemonData(response.data);
+
+            } catch (e) {
+                console.error(e);
+
+            }
+        }
+
+        getPokemon();
+    }, []);
+
+
+    return (
+        <>
+            <h1>Gotta catch em all!</h1>
+        </>
+    )
 }
 
 export default App
